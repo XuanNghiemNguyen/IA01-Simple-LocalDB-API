2b62b35d82b2fdfe09c8b9c69b8731ea702b27e8
XuanNghiemNguyen <xuanghjem@gmail.com>
2020-04-07 22:11:38 +0700

    revert

--- Phap-Anh/app.js
+++ Phap-Anh/app.js
@@ -6,9 +6,7 @@ const adapter = new FileSync('./database/phap-anh.json')
 const db = low(adapter)
 const morgan = require('morgan')
 var bodyParser = require('body-parser')
-var cors = require('cors')
- 
-app.use(cors())
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
@@ -30,22 +28,13 @@ app.get('/translate/fr-en', (req, res) => {
   try {
     const { keyword } = req.query
     if (!keyword) {
-      res.status(200).json({
-        title: 'TỪ ĐIỂN PHÁP-ANH',
-        message: `Hãy nhập từ khóa!`,
-      })
-      return
+      throw new Error('Hãy nhập từ khóa!')
     }
     const keySearch = new RegExp(keyword.toLowerCase())
     let results = db.get('words').value()
     results = results.filter(item => item.french.toLowerCase().match(keySearch))
     if (!results.length) {
-      res.status(200).json({
-        title: 'TỪ ĐIỂN PHÁP-ANH',
-        message: `Không tìm thấy kết quả nào với từ: '${keyword}'`,
-        count: 0
-      })
-      return
+      throw new Error(`Không tìm thấy kết quả nào với từ: '${keyword}'`)
     }
     results = results.map(item => {
       return (
@@ -58,7 +47,6 @@ app.get('/translate/fr-en', (req, res) => {
     res.status(200).json({
       title: 'TỪ ĐIỂN PHÁP-ANH',
       message: `Tìm thấy ${results.length} kết quả cho từ khóa '${keyword}':`,
-      count: results.length,
       results
     })
   } catch (error) {
@@ -73,22 +61,13 @@ app.get('/translate/en-fr', (req, res) => {
   try {
     const { keyword } = req.query
     if (!keyword) {
-      res.status(200).json({
-        title: 'TỪ ĐIỂN ANH-PHÁP',
-        message: `Hãy nhập từ khóa!`,
-      })
-      return
+      throw new Error('Hãy nhập từ khóa!')
     }
     const keySearch = new RegExp(keyword.toLowerCase())
     let results = db.get('words').value()
     results = results.filter(item => item.english.toLowerCase().match(keySearch))
     if (!results.length) {
-      res.status(200).json({
-        title: 'TỪ ĐIỂN ANH-PHÁP',
-        message: `Không tìm thấy kết quả nào với từ: '${keyword}'`,
-        count: 0
-      })
-      return
+      throw new Error(`Không tìm thấy kết quả nào với từ: '${keyword}'`)
     }
     results = results.map(item => {
       return (
@@ -101,7 +80,6 @@ app.get('/translate/en-fr', (req, res) => {
     res.status(200).json({
       title: 'TỪ ĐIỂN ANH-PHÁP',
       message: `Tìm thấy ${results.length} kết quả cho từ khóa '${keyword}':`,
-      count: results.length,
       results
     })
   } catch (error) {
